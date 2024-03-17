48e4158aae3b18f7a98bca0b4bb1528a007e0622
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-03-17 22:06:14 +0300

    dark theme

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -5,5 +5,8 @@ ThemeData lightMode = ThemeData(
     background: Colors.grey.shade300,
     primary: Colors.grey.shade500,
     secondary: Colors.grey.shade100,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade700,
+    
   ),
 );
