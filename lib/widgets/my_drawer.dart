884af0dbf099556fc4ea2fe08706aba20dcfa703
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-03-18 12:47:56 +0300

    settings page

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:food_delivery/pages/settings_page.dart';
+import 'package:food_delivery/widgets/primary_text.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -7,7 +8,79 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: [],),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Icon(
+                Icons.dinner_dining_rounded,
+                size: 72,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            Divider(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            MyDrawerTile(
+              iconData: Icons.home,
+              text: 'H O M E',
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            MyDrawerTile(
+              iconData: Icons.settings,
+              text: 'S E T T I N G S',
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SettingsPage(),
+                  ),
+                );
+              },
+            ),
+            Spacer(),
+            MyDrawerTile(
+              iconData: Icons.logout,
+              text: 'L O G O U T',
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyDrawerTile extends StatelessWidget {
+  final String text;
+  final Function() onTap;
+  final IconData iconData;
+  const MyDrawerTile({
+    super.key,
+    required this.text,
+    required this.iconData,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      leading: Icon(
+        iconData,
+        color: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      title: PrimaryText(
+        text: text,
+        color: Theme.of(context).colorScheme.inversePrimary,
+      ),
     );
   }
 }
