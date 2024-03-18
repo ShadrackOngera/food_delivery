8cce1c9bb3dd7cdd002877357df27c5f554b439f
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-03-18 21:31:51 +0300

    settings page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:food_delivery/themes/theme_provider.dart';
+import 'package:food_delivery/widgets/primary_text.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -11,6 +14,47 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        foregroundColor: Theme.of(context).colorScheme.primary,
+        title: PrimaryText(
+          text: 'Settings',
+          color: Theme.of(context).colorScheme.inversePrimary,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 13),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  PrimaryText(
+                    text: 'Dark mode',
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  CupertinoSwitch(
+                    value: Provider.of<ThemeProvider>(context, listen: false)
+                        .isDarkMode,
+                    onChanged: (v) =>
+                        Provider.of<ThemeProvider>(context, listen: false)
+                            .toggleTheme(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
