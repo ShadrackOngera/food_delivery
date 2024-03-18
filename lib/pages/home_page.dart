18bac96182ba7fbe8b54c5421a72633ff4fd43bf
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-03-18 21:33:30 +0300

    home appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/widgets/my_drawer.dart';
+import 'package:food_delivery/widgets/primary_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +13,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        foregroundColor: Theme.of(context).colorScheme.primary,
+        centerTitle: true,
+        title: PrimaryText(
+          text: 'Home',
+          color: Theme.of(context).colorScheme.inversePrimary,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
       drawer: MyDrawer(),
       body: SingleChildScrollView(),
     );
