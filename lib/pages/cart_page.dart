cdce529cec72cecbd7d77e91abcc8167dd497658
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-04-03 01:08:10 +0300

    clear cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -25,6 +25,44 @@ class CartPage extends StatelessWidget {
               text: 'Cart',
               color: Theme.of(context).colorScheme.inversePrimary,
             ),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: PrimaryText(
+                        text: 'Are you sure you want to clear cart?',
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: PrimaryText(
+                            text: 'Cancel',
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            restaurant.clearCart();
+                          },
+                          child: PrimaryText(
+                            text: 'Yes',
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.delete,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              )
+            ],
           ),
           body: Column(
             children: [
@@ -113,39 +151,52 @@ class MyCartTile extends StatelessWidget {
             //addons
             SizedBox(
               height: cartItem.selectedAddons.isEmpty ? 0 : 60,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: cartItem.selectedAddons
-                    .map(
-                      (addon) => FilterChip(
-                        label: Row(
-                          children: [
-                            PrimaryText(
-                              text: addon.name,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 7,
+                  bottom: 9,
+                  right: 7,
+                ),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: cartItem.selectedAddons
+                      .map(
+                        (addon) => Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: FilterChip(
+                            label: Row(
+                              children: [
+                                PrimaryText(
+                                  text: addon.name,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary,
+                                ),
+                                PrimaryText(
+                                  text: "Ksh${addon.price}",
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary,
+                                ),
+                              ],
+                            ),
+                            onSelected: (value) {},
+                            shape: StadiumBorder(
+                              side: BorderSide(
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                            backgroundColor:
+                                Theme.of(context).colorScheme.secondary,
+                            labelStyle: TextStyle(
                               color:
                                   Theme.of(context).colorScheme.inversePrimary,
                             ),
-                            PrimaryText(
-                              text: "Ksh${addon.price}",
-                              color:
-                                  Theme.of(context).colorScheme.inversePrimary,
-                            ),
-                          ],
-                        ),
-                        onSelected: (value) {},
-                        shape: StadiumBorder(
-                          side: BorderSide(
-                            color: Theme.of(context).colorScheme.primary,
                           ),
                         ),
-                        backgroundColor:
-                            Theme.of(context).colorScheme.secondary,
-                        labelStyle: TextStyle(
-                          color: Theme.of(context).colorScheme.inversePrimary,
-                        ),
-                      ),
-                    )
-                    .toList(),
+                      )
+                      .toList(),
+                ),
               ),
             ),
           ],
